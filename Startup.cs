939bc47de16e033ce7e69006407cd354ab2d662d
Darren Neimke <darren.neimke@live.com>
2019-08-30 09:53:41 +0930

    .

--- Startup.cs
+++ Startup.cs
@@ -39,7 +39,7 @@ namespace StripeSample
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env, ApplicationDbContext dbContext)
         {
-            // dbContext.Database.Migrate();
+            dbContext.Database.Migrate();
 
             if (env.IsDevelopment())
             {
