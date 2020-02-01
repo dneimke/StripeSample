6d768aac442d10a69ad77cd20bad176454f48763
Darren Neimke <darren.neimke@live.com>
2020-02-01 15:03:27 +1030

    .

--- src/StripeSample/UserContext.cs
+++ src/StripeSample/UserContext.cs
@@ -1,5 +1,4 @@
-﻿using StripeSample.Domain;
-using StripeSample.Infrastructure.Data;
+﻿using StripeSample.Infrastructure.Data;
 using System;
 using System.Linq;
 
@@ -7,29 +6,20 @@ namespace StripeSample
 {
     public class UserContext
     {
-        private readonly SubscriptionsContext _dbContext;
-
-        public Guid Id { get; }
-        public string EmailAddress { get; }
-        public string CustomerId { get; }
+        public Guid Id { get; } = Guid.Empty;
+        public string EmailAddress { get; } = "";
+        public string CustomerId { get; } = "";
+        public bool IsAuthenticated => Id != Guid.Empty;
 
         public UserContext(SubscriptionsContext dbContext)
         {
-            var user = dbContext.ApplicationUser.FirstOrDefault(e => e.Id == Guid.Parse("07b742cc-8c82-43b6-8615-de54635db929"));
+            var user = dbContext.ApplicationUser.SingleOrDefault(e => e.Id == Guid.Parse("07b742cc-8c82-43b6-8615-de54635db929"));
             if(user != null)
             {
                 Id = user.Id;
                 EmailAddress = user.EmailAddress;
                 CustomerId = user.CustomerId;
             }
-
-            _dbContext = dbContext;
         }
-
-        public ApplicationUser GetUser()
-        {
-            var user = _dbContext.ApplicationUser.FirstOrDefault(e => e.Id == Id);
-            return user;
-        } 
     }
 }
